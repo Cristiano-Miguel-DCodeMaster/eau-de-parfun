1440683f8ecbff7e9b7ccfd2bd59a731b2b6ef3e
Cristiano-Miguel-DCodeMaster <cristianomiguel128@gmail.com>
2024-04-10 15:56:30 +0100

    I added some interaction with JavaScript

--- js/app.js
+++ js/app.js
@@ -2,3 +2,28 @@ setTimeout(() => {
     const that = document.getElementById('that');
     that.classList.remove('that');
 }, 2000);
+
+setTimeout(() => {
+    const h1 = document.querySelector('h1');
+    h1.style.opacity = "1";
+}, 2200);
+
+setTimeout(() => {
+    const h2 = document.querySelector('h2');
+    h2.style.opacity = "1";
+}, 2500);
+
+setTimeout(() => {
+    const flora = document.querySelector('.flora');
+    flora.style.opacity = "1";
+}, 2700);
+
+setTimeout(() => {
+    const price = document.querySelector('.price');
+    price.style.opacity = "1";
+}, 2700);
+
+setTimeout(() => {
+    const button = document.querySelector('button');
+    button.style.opacity = "1";
+}, 3100);
